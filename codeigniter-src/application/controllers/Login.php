299260c918b0fa5d5dfbf3f323a0392b6fab1508
baubyte <paredbaez.martin@gmail.com>
2021-04-29 00:35:41 -0300

    Usando Helpers

--- codeigniter-src/application/controllers/Login.php
+++ codeigniter-src/application/controllers/Login.php
@@ -3,9 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper(['getmenu', 'url']);
+	}
 	public function index()
 	{
-		$this->load->view('login');
+		$data['menus'] = mainMenu();
+		$this->load->view('login',$data);
 	}
 
 }
--- codeigniter-src/application/controllers/Registro.php
+++ codeigniter-src/application/controllers/Registro.php
@@ -3,8 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Registro extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper(['getmenu', 'url']);
+	}
 	public function index()
 	{
-		$this->load->view('registro');
+		$data['menus'] = mainMenu();
+		$this->load->view('registro',$data);
 	}
 }
