2b546324d4d593f342c3b86f629449eb1cb024b4
baubyte <paredbaez.martin@gmail.com>
2021-04-29 00:36:05 -0300

    Listando menus

--- codeigniter-src/application/views/registro.php
+++ codeigniter-src/application/views/registro.php
@@ -13,7 +13,11 @@
 
 <body>
 	<h1>Registro</h1>
-
+	<ul>
+		<?php foreach($menus as $menu): ?>
+			<li><a href="<?=$menu['url']?>"><?=$menu['title']?></a></li>
+		<?php endforeach;?>
+	</ul>
 	<!-- JQuery -->
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
 	<!-- Bootstrap -->
