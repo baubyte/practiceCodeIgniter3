7e2d43e0326b6ed107ab9e3a211fd9656b25159c
baubyte <paredbaez.martin@gmail.com>
2021-04-29 22:41:30 -0300

    create usuario

--- codeigniter-src/application/controllers/Registro.php
+++ codeigniter-src/application/controllers/Registro.php
@@ -8,22 +8,36 @@ class Registro extends CI_Controller {
 		parent::__construct();
 		$this->load->helper(['getmenu']);
 		$this->load->database();
+		$this->load->model('User');
 	}
 
 	public function index()
 	{
 		$data['menus'] = mainMenu();
 		$this->load->view('registro',$data);
-		$query = $this->db->get('usuarios');
-		var_dump($query->result());
 	}
 
 	public function create()
 	{
+		$data['menus'] = mainMenu();
+
 		$username = $this->input->post('username');
 		$email = $this->input->post('email');
 		$password = $this->input->post('password');
 		$cpassword = $this->input->post('cpassword');
-		var_dump($this->input->post());
+
+
+		$dataUser = [
+			'nombre_usuario' => $username,
+			'email' => $email,
+			'password' => $cpassword
+		];
+		/**Usamos el modelo */
+		if(!$this->User->create($dataUser)){
+			$data['msg']= 'Ocurrió un error al intentar ingresar los datos.';
+			$this->load->view('registro',$data);
+		}
+		$data['msg']= 'Éxito se Registro correctamente.';
+		$this->load->view('registro',$data);
 	}
 }
